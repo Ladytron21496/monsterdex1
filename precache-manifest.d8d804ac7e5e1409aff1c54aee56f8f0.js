b1dea99f70e36576520621af63210fccfd5a3663
Busetty <Apuroop.Busetty@shell.com>
2019-11-14 18:41:48 +0530

    Updates

---
+++ precache-manifest.d8d804ac7e5e1409aff1c54aee56f8f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7cdfd274c501c3b24a0be9d1df43ec6",
+    "url": "/monsterdex1/index.html"
+  },
+  {
+    "revision": "c98e16e46c8141e2fa1f",
+    "url": "/monsterdex1/static/css/main.0bcb9693.chunk.css"
+  },
+  {
+    "revision": "391cb0e4e182aa14b4b0",
+    "url": "/monsterdex1/static/js/2.5430253d.chunk.js"
+  },
+  {
+    "revision": "c98e16e46c8141e2fa1f",
+    "url": "/monsterdex1/static/js/main.0fbb7702.chunk.js"
+  },
+  {
+    "revision": "eb56d86f2248ca620b39",
+    "url": "/monsterdex1/static/js/runtime-main.60b99f86.js"
+  }
+]);
